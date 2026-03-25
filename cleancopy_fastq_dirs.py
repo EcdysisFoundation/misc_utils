bbf5a5313d014ccb7ca9adc7b2e201cb9565dd5c
Mikayla Wilson <mikaylaelectra@gmail.com>
2026-03-25 14:12:06 -0400

    fastq files need removed from .zip directories

---
+++ cleancopy_fastq_dirs.py
@@ -0,0 +1,74 @@
+############
+## clean and copy fastq files to a new directory for processing
+## fastq files need removed from .zip directories
+## omits unneccessary files and retains original directory structure
+############
+import os
+import zipfile
+import re
+import shutil
+
+
+STARTING_LOCATION = '/pool1/smb/fastq/ALL years fastQ'
+ENDING_LOCATION = '/pool1/srv/fastq'
+YEAR_DIRS = ['2022', '2023', '2024']
+
+# Match a space, an open parenthesis, one or more digits, and a closing parenthesis
+duplicate_pattern = r' \(\d+\)'
+
+def make_dir(dir_to_make):
+    new_dir = ENDING_LOCATION + f'/{dir_to_make}'
+    if os.path.exists(new_dir):
+        shutil.rmtree(new_dir)
+        os.makedirs(new_dir)
+
+
+def handle_zipfile(zip_path, save_to_dir):
+
+    with zipfile.ZipFile(zip_path, 'r') as z:
+        for member in z.infolist():
+            # Skip directories
+            if member.is_dir():
+                continue
+            if not member.filename.endswith('.fastq.gz'):
+                print(f'Skipping: {member.filename}')
+                continue
+            if re.search(duplicate_pattern, member.filename):
+                print(f'Skipping duplicate: {member.filename}')
+                continue
+
+            filename = os.path.basename(member.filename)
+            target_path = os.path.join(save_to_dir, filename)
+            with z.open(member) as source, open(target_path, "wb") as target:
+                shutil.copyfileobj(source, target)
+
+
+def do_main():
+
+    zips_processed = 0
+
+    for year in YEAR_DIRS:
+        # stop early
+        if zips_processed > 0:
+            break
+        print(f'processing year {year}')
+
+        make_dir(year)
+
+        zip_dir = STARTING_LOCATION + f'/{year}'
+        zip_files = [f for f in os.listdir(zip_dir) if f.endswith('.zip')]
+        year_dir = ENDING_LOCATION + f'/{year}'
+
+        print(f"Found {len(zip_files)} ZIP archives. Starting extraction...")
+        for zip_name in zip_files:
+            print(f'working on {zip_name}')
+            save_to_dir = year_dir + zip_name.replace('.zip', '')
+            make_dir(save_to_dir)
+            zip_path = os.path.join(zip_dir, zip_name)
+            handle_zipfile(zip_path, save_to_dir)
+            # stop early
+            if zips_processed > 0:
+                break
+
+if __name__ == '__main__':
+    do_main()
