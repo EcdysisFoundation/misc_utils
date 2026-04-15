057a65dad221bfcdc1416d042bb2e21ac3942bcd
Mikayla Wilson <mikaylaelectra@gmail.com>
2026-04-15 12:47:48 -0400

    relative imports ok

--- data_mods/back_populate_label_data.py
+++ data_mods/back_populate_label_data.py
@@ -1,25 +1,17 @@
 import os
 
-from gen_utils import extract_guid
-from stitcher_api import post_sent_ls
+from ..gen_utils import extract_guid
+from ..stitcher_api import post_sent_ls
 
 ####################################################
 ### Backpopluate fields used in cvat_utils.py
 ### These are fields that did not formerly exist
 ### TASK_NAME is the cvat-tasks directory to search
+### python -m misc_utils.data_mods.back_populate_label_data
 ####################################################
 
 TASK_NAME = 'sdk_test'
 PROJECT_NAME = "SDK Test Project"
-
-#'WA on CVAT'
-#'wa_on_cvat'
-
-#'Texas/Oklahoma 2025c'
-#'texas_ok_d_2025'
-
-#'Maine 2025'
-#'maine_2025'
 
 DATA_DIR = f'/pool1/srv/cvat-tasks/{TASK_NAME}'
 
@@ -38,3 +30,7 @@ def update_recs_from_directory():
             'label_file': v[1]
         }
         post_sent_ls(post_params)
+
+
+if __name__ == '__main__':
+    update_recs_from_directory()
