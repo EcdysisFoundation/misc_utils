dfbb2c245b3fa0b5088a700d2304f28573087333
Mikayla Wilson <mikaylaelectra@gmail.com>
2026-06-26 10:06:27 -0400

    back_populate_label_data for label_studio_conversions scenario

--- data_mods/back_populate_label_data.py
+++ data_mods/back_populate_label_data.py
@@ -1,34 +1,54 @@
+import argparse
 import os
+import time
 
 from ..gen_utils import extract_guid
-from ..stitcher_api import post_sent_ls
+from ..stitcher_api import post_sent_ls, updated_label_post
 
 ####################################################
-### Backpopluate fields used in cvat_utils.py
-### These are fields that did not formerly exist
-### TASK_NAME is the cvat-tasks directory to search
-### python -m misc_utils.data_mods.back_populate_label_data
+### Backpopluate fields used in cvat_utils.py,
+### task_dir is the cvat-tasks directory to search,
+### example use case, for task not actually on cvat
+### python -m misc_utils.data_mods.back_populate_label_data --task-dir label_studio_conversions
 ####################################################
 
-TASK_NAME = 'sdk_test'
-PROJECT_NAME = "SDK Test Project"
 
-DATA_DIR = f'/pool1/srv/cvat-tasks/{TASK_NAME}'
+def get_args() -> argparse.Namespace:
+
+    parser = argparse.ArgumentParser(description='Dataset generation')
+    parser.add_argument(
+        '--task-dir', type=str, required=True,
+        help='The task dir in DATA_DIR')
+    parser.add_argument(
+        '--data-dir', type=str, default='/pool1/srv/cvat-tasks/',
+        help='The task dir in DATA_DIR')
+    return parser.parse_args()
 
 
-def update_recs_from_directory():
+def update_recs_from_directory(args):
 
-    label_files = sorted([f for f in os.listdir(DATA_DIR) if f.endswith(('.txt'))])
+    label_files = sorted([f for f in os.listdir(args.data_dir) if f.endswith(('.txt'))])
     guid_map = [(extract_guid(i), i) for i in label_files]
     for v in guid_map:
         post_params = {
             'guid': v[0],
-            'project': PROJECT_NAME,
-            'label_project_dir': TASK_NAME,
-            'label_file': v[1]
+            'project': args.task_dir,
+            'label_project_dir': args.task_dir,
+            'label_file': v[1],
+            'label_job_id': 0,  # no job, required field
+            'label_task_id': 0  # no id, required field
         }
         post_sent_ls(post_params)
+    return guid_map
 
 
 if __name__ == '__main__':
-    update_recs_from_directory()
+    args = get_args()
+    print(f"update_recs_from_directory...{args.data_dir}")
+    guid_map = update_recs_from_directory(args)
+    time.sleep(60)
+    for guid in guid_map:
+        post_params = {
+            'guid': guid[0]
+        }
+        updated_label_post(post_params)
